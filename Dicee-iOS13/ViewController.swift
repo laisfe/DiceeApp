ec9207c2c4b0b28c0401f9e6f296eb2f76e015bf
Lais Fialho <laisffn@hotmail.com>
2023-02-05 00:54:27 -0300

    Add IBOutlets and IBAction and randomize dices

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDice = 0
+    var rightDice = 5
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        diceImageViewOne.image = diceArray.randomElement() //or [Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray.randomElement() //or [Int.random(in: 0...5)]
     }
-
-
+    
 }
 
